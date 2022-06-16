a49c9d9f16ac587ca84711a89cff9115d7f27ac0
RenzoBTL30 <90786322+RenzoBTL30@users.noreply.github.com>
2022-06-15 20:39:46 -0500

    pantalla-inicio

---
+++ src/app/pantalla-presentacion/pantalla-presentacion.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PantallaPresentacionComponent } from './views/pantalla-presentacion/pantalla-presentacion.component';
+
+
+
+@NgModule({
+  declarations: [
+    PantallaPresentacionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PantallaPresentacionModule { }
---
+++ src/app/pantalla-presentacion/views/pantalla-presentacion/pantalla-presentacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-presentacion',
+  templateUrl: './pantalla-presentacion.component.html',
+  styleUrls: ['./pantalla-presentacion.component.css']
+})
+export class PantallaPresentacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
