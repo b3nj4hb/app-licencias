533b786bb1734f78b1e17d4077adb0ae49e6191b
Digi85 <73599904+Digi85@users.noreply.github.com>
2022-06-29 18:40:53 -0500

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'storage-voucher',
+    appId: '1:905154503591:web:362d21ef9650a5888d8c82',
+    storageBucket: 'storage-voucher.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDatxoEMgaRcN0yi-z8pt0p-OiirdIh4jg',
+    authDomain: 'storage-voucher.firebaseapp.com',
+    messagingSenderId: '905154503591',
+    measurementId: 'G-J1G1VDLF09',
+  },
   production: false
 };
 /*
