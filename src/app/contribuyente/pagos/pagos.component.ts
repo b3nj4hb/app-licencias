299e8939ed341fbc0e24705310515fa3f4c734dd
Digi85 <73599904+Digi85@users.noreply.github.com>
2022-06-29 19:01:35 -0500

    subir imagen listo

--- src/app/contribuyente/pagos/pagos.component.ts
+++ src/app/contribuyente/pagos/pagos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Storage, ref ,uploadBytes } from '@angular/fire/storage'; 
 @Component({
   selector: 'app-pagos',
   templateUrl: './pagos.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storage: Storage) { }
 
   ngOnInit(): void {
   }
+  uploadImage($event: any){
+    const file = $event.target.files[0];
+    console.log(file);
 
+    const imgRef = ref(this.storage,`images/${file.name}`);
+    
+    uploadBytes(imgRef, file)
+    .then()
+    .catch(error => console.log(error));
+  }
 }
