72fd5817694200e30b262487f60fcdbde21b6350
Digi85 <73599904+Digi85@users.noreply.github.com>
2022-06-29 20:33:25 -0500

    cambios rndom

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewAnexosComponent } from './view-anexos/view-anexos.component';
+import { ViewDecjuComponent } from './view-decju/view-decju.component';
+import { ViewVoucherComponent } from './view-voucher/view-voucher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'view-voucher', component: ViewVoucherComponent},
+  {path: 'view-decju', component: ViewDecjuComponent},
+  {path: 'view-anexos', component: ViewAnexosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
