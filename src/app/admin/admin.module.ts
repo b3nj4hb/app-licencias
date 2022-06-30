db00bda0ae35dbfe960738cee1a79e7d37b34c3e
Digi85 <73599904+Digi85@users.noreply.github.com>
2022-06-29 21:48:07 -0500

    vista voucher

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 import { ViewVoucherComponent } from './view-voucher/view-voucher.component';
 import { ViewAnexosComponent } from './view-anexos/view-anexos.component';
 import { ViewDecjuComponent } from './view-decju/view-decju.component';
+import { ComponentsModule } from '../components/components.module';
 
 
 @NgModule({
@@ -15,7 +15,8 @@ import { ViewDecjuComponent } from './view-decju/view-decju.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ComponentsModule,
   ]
 })
 export class AdminModule { }
--- src/app/admin/view-voucher/view-voucher.component.ts
+++ src/app/admin/view-voucher/view-voucher.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Storage, ref,listAll, getDownloadURL } from '@angular/fire/storage';
 @Component({
   selector: 'app-view-voucher',
   templateUrl: './view-voucher.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewVoucherComponent implements OnInit {
 
-  constructor() { }
+  images: string [];
 
-  ngOnInit(): void {
+  constructor(private storage: Storage){
+    this.images=[];
   }
 
+  ngOnInit() {
+    this.getImages();
+  }
+  
+  getImages(){
+    const imagesRef =ref(this.storage, 'images');
+
+    listAll(imagesRef)
+    .then(async response =>{
+      console.log(response);
+      this.images=[];
+      for (let item of response.items){
+        const url = await getDownloadURL(item);
+        this.images.push(url)
+      }
+    })
+    .catch(error => console.log(error))
+  }
 }
