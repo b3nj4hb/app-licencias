fd9a26eac06fb6d37a774354e2ffc9b0fbaf7843
b3nj4hb <benjalive12@gmail.com>
2022-06-29 22:41:31 -0500

    vista contribuyente

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewAnexosComponent } from './view-anexos/view-anexos.component';
+import { ViewContribuyenteComponent } from './view-contribuyente/view-contribuyente.component';
 import { ViewDecjuComponent } from './view-decju/view-decju.component';
 import { ViewVoucherComponent } from './view-voucher/view-voucher.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path: 'view-voucher', component: ViewVoucherComponent},
   {path: 'view-decju', component: ViewDecjuComponent},
   {path: 'view-anexos', component: ViewAnexosComponent},
+  {path: 'view-contribuyente', component: ViewContribuyenteComponent},
 ];
 
 @NgModule({
---
+++ src/app/admin/view-contribuyente/view-contribuyente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-contribuyente',
+  templateUrl: './view-contribuyente.component.html',
+  styleUrls: ['./view-contribuyente.component.css']
+})
+export class ViewContribuyenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
