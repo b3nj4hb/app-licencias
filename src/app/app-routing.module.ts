d5fcf17f764c9858caa86bbdb8d7a24125d0f155
b3nj4hb <benjalive12@gmail.com>
2022-07-20 16:42:08 -0500

    firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    // RouterModule.forRoot(routes, {useHash: true}),
+    RouterModule.forRoot(routes, {useHash: true}),
     RouterModule.forRoot(routes),
     ComponentsModule,
     CoreModule
